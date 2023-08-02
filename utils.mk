85fe5720775cfe5b762fc421768efb73468a22c4
Alexandr Savca <alexandr.savca89@gmail.com>
2023-08-02 06:21:36 +0300

    utils.mk: rework

--- utils.mk
+++ utils.mk
@@ -1,26 +1,24 @@
-GREPOPT = --exclude-dir=.git --exclude-dir=.github -R .
-FINDOPT = -not \( -path "./.git*" -or -path ".*~" \)
-MAXLINE = 80
+all: deadlinks podchecker shellcheck longlines
 
 deadlinks:
 	@echo "=======> Check for dead links"
-	@grep -Eiho "https?://[^\"\\'> ]+" ${GREPOPT} \
-		| xargs -P10 -I{} curl -o /dev/null   \
-		 -sw "[%{http_code}] %{url}\n" '{}'   \
-		| grep -v '^\[200\]'                  \
+	@grep -EIihor "https?://[^\"\\'> ]+" --exclude-dir=.git*  \
+		| xargs -P10 -r -I{} curl -I -o/dev/null          \
+		  -sw "[%{http_code}] %{url}\n" '{}'              \
+		| grep -v '^\[200\]'                              \
 		| sort -u
 
 podchecker:
 	@echo "=======> Check PODs for syntax errors"
-	@podchecker *.pod
+	@podchecker *.pod >/dev/null
 
 shellcheck:
 	@echo "=======> Check shell scripts for syntax errors"
-	@grep -m1 -l '^#\s*!/bin/sh' ${GREPOPT} | xargs -L10 shellcheck -s sh
+	@grep -m1 -Irl '^#\s*!/bin/sh' --exclude-dir=.git* \
+		| xargs -L10 -r shellcheck -s sh
 
 longlines:
-	@echo "=======> Check for long lines (> ${MAXLINE})"
-	@find . -type f ${FINDOPT} -exec awk -v ML=${MAXLINE} \
-		'length > ML { print FILENAME ":" FNR " " $$0 }'  {} \;
+	@echo "=======> Check for long lines"
+	@! grep -PIrn '^.{81,}$$' --exclude-dir=.git*
 
-.PHONY: deadlinks podchecker shellcheck longlines
+.PHONY: all deadlinks podchecker shellcheck longlines
