9c5e11e3f78d7369dbe5ff77790d65a39bc244d0
Alexandr Savca <alexandr.savca89@gmail.com>
2023-08-17 19:18:03 +0300

    config.mk: add HOMEPAGE and BUGTRACKER variables

--- config.mk
+++ config.mk
@@ -1,6 +1,8 @@
 # project metadata
 NAME         = rejmerge
 VERSION      = 5.44
+HOMEPAGE     = https://github.com/zeppe-lin/rejmerge/
+BUGTRACKER   = https://github.com/zeppe-lin/rejmerge/issues/
 DIST         = ${NAME}-${VERSION}
 
 # paths
